c5ff563c9cd05c6b5789bd6459914f2ed1ccaa4c
Ben Schinn <schinn.ben@gmail.com>
2021-03-01 21:31:10 -0500

    finished

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,17 +7,38 @@ export default function Details(props) {
   const [details, setDetails] = useState(null)
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+  // synchronize your state to no state
+  useEffect(() => {
+    console.log('🥇')
+  }, [])
 
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
+  useEffect(() => {
+    const clickHandler = () => console.log('👻')
+    document.addEventListener('click', clickHandler)
+
+    //cleanup
+    return () => {
+      document.removeEventListener('click', clickHandler)
+    }
+  })
 
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+  // synchronized to all state changes/every render
+  useEffect(() => {
+    console.log('🥵')
+  })
 
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+  useEffect(() => {
+    axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+    .then(({data}) => setDetails(data))
+  }, [friendId])
 
   return (
     <div className='container'>
